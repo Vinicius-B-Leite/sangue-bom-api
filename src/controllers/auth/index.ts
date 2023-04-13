5b805034f40e2d34367d6771ec7d3ecc41482c82
Vinicius B. Leite1 <viniciusbleite21@gmail.com>
2023-04-13 20:53:05 -0300

    feature: efetuando o login

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -20,7 +20,43 @@ class AuthController {
         return res.json({ ...user, token })
     }
 
+    async login(req: Request, res: Response) {
+        const { email, password } = req.body
 
+        const user = await prismaClient.users.findFirst({
+            where: {
+                email
+            }
+        })
+
+        if (!user) {
+            const bloodCollectors = await prismaClient.bloodCollectors.findFirst({
+                where: {
+                    email
+                }
+            })
+
+            if (!bloodCollectors){
+                return res.json({error: 'Nenhum usuário encontrado'})
+            }
+
+            if (bloodCollectors.password !== password){
+                return res.json({error: 'Sennha incorreta'})
+            }
+
+            const token = jwt.sign({ uid: bloodCollectors.uid }, process.env.JWT_PASS ?? '', { expiresIn: '15d' })
+            return res.json({...bloodCollectors, token})
+        }
+
+        if (user.password !== password){
+            return res.json({error: 'Senha incorreta'})
+        }
+
+        const token = jwt.sign({ uid: user.uid }, process.env.JWT_PASS ?? '', { expiresIn: '15d' })
+        return res.json({...user, token})
+
+
+    }
 }
 
 export const authController = new AuthController()
--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,8 @@ import { bloodCollectorsController } from "./controllers/bloodCollectors";
 const router = Router()
 
 router.post('/auth/create', authController.store)
-
 router.post('/bloodcollectors/create', bloodCollectorsController.store)
+
+router.post('/auth/login', authController.login)
 
 export default router
