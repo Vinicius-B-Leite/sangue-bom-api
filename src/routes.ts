234f5038643209dbdbbe45a22f266cd7ce0cbab3
Vinicius B. Leite1 <viniciusbleite21@gmail.com>
2023-04-13 21:02:27 -0300

    feature: middleware que verifica se o usuario esta logado

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authController } from "./controllers/auth";
 import { bloodCollectorsController } from "./controllers/bloodCollectors";
+import { isLogged } from "./middleware/isLoged";
 
 const router = Router()
 
@@ -8,5 +9,8 @@ router.post('/auth/create', authController.store)
 router.post('/bloodcollectors/create', bloodCollectorsController.store)
 
 router.post('/auth/login', authController.login)
+
+
+router.get('/', isLogged)
 
 export default router
