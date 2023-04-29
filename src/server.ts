ff3b7d221fa62d841fc9f5ee38d308eb4980301a
Vinicius B. Leite1 <viniciusbleite21@gmail.com>
2023-04-29 14:53:47 -0300

    bugfixes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import 'express-async-errors'
 import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import router from './routes'
-
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
